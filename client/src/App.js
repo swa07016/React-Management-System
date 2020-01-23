90f88a15e8acf0ed293d1016eebbe354640b14a8
swa07016 <swa07016@naver.com>
2020-01-24 00:57:05 +0900

    Apply Circular Progress Loading Bar

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,6 +18,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  }, 
+  progress: {
+    margin: theme.spacing.unit*2
   }
 });
 
@@ -24,19 +28,30 @@ const styles = theme => ({
 class App extends Component {
   
   state = {
-    customers:''
+    customers:'',
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers:res}))
     .catch(err => console.log(err));
   } 
 
+  componentWillMount() {
+    clearInterval(this.timer);
+  }
+
   callApi = async ()=> {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed:completed >= 100 ? 0 : completed + 1});
   }
 
   render() {
@@ -66,7 +81,13 @@ class App extends Component {
               gender={customer.gender}
               job={customer.job}
             />
-          }):''}
+          }):
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+          }
         </TableBody>
       </Table>
       </Paper>
