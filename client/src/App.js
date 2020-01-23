a89e2fd7d92f99b73c2dd15f96092e36562ab454
swa07016 <swa07016@naver.com>
2020-01-24 00:20:23 +0900

    Apply node.js Express [/api/customers/]

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,25 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '나동빈',
-    'birthday': '960508',
-    'gender': '남자',
-    'job': '프로그래머'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이순신',
-    'birthday': '961127',
-    'gender': '남자',
-    'job': '디자이너'
-  }
-]
 
 class App extends Component {
+  
+  state = {
+    customers:''
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  } 
+
+  callApi = async ()=> {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -65,7 +56,7 @@ class App extends Component {
         </TableHead>
 
         <TableBody>
-          {customers.map(customer => {
+          {this.state.customers ? this.state.customers.map(customer => {
             return <Customer
               key={customer.id}
               id={customer.id}
@@ -75,7 +66,7 @@ class App extends Component {
               gender={customer.gender}
               job={customer.job}
             />
-          })}
+          }):''}
         </TableBody>
       </Table>
       </Paper>
